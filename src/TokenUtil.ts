21cb56b1f7f85c46dd177bcd6c26b1fcc2395617
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-07 11:54:01 +0100

    Add a method to get a username from a token

--- src/TokenUtil.ts
+++ src/TokenUtil.ts
@@ -1,4 +1,6 @@
+import { HelixUser } from "@twurple/api";
 import { AccessToken } from "@twurple/auth";
+import axios from "axios";
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 
@@ -6,7 +8,9 @@ export default class TokenUtil {
   private readonly PATH_TO_TOKEN = "../authToken.json";
   private static instance: TokenUtil = new TokenUtil();
 
-  private constructor() {}
+  public constructor(clientId: string) {
+    this.clientId = clientId;
+  }
 
   public static getInstance(): TokenUtil {
     return this.instance;
@@ -23,5 +27,18 @@ export default class TokenUtil {
       join(__dirname, this.PATH_TO_TOKEN),
       JSON.stringify(accessToken)
     );
+  }
+  public async getUsernameByAccessToken(accessToken: AccessToken): Promise<string> {
+    const userInfo = await axios.get<{ data: HelixUser[] }>(
+      "https://api.twitch.tv/helix/users",
+      {
+        headers: {
+          "Client-Id": this.clientId,
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return userInfo.data.data[0].displayName.toLowerCase();
   }
 }
