e122d814e0f7b4bd510aed8d32806d281664b14f
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-08 13:18:14 +0100

    Add an ObjectManager class

---
+++ src/ObjectManager.ts
@@ -0,0 +1,30 @@
+import AuthManager from "./AuthManager";
+import ConfigInitializer from "./ConfigInitializer";
+import HttpServer from "./HttpServer";
+import ManageableClass from "./ManageableClass";
+import TokenUtil from "./TokenUtil";
+import TwitchClient from "./TwitchClient";
+
+export default class ObjectManager {
+    private manageableClasses: Map<string, ManageableClass> = new Map();
+
+    public constructor(configInitializer: ConfigInitializer) {
+        const config = configInitializer.getConfig();
+
+        this.manageableClasses.set(AuthManager.name, new AuthManager(config.twitchAppClientId, config.twitchAppClientSecret));
+        this.manageableClasses.set(HttpServer.name, new HttpServer(config.httpServerPort));
+        this.manageableClasses.set(TwitchClient.name, new TwitchClient((this.manageableClasses.get(AuthManager.name) as AuthManager).getAuthProvider()));
+        this.manageableClasses.set(TokenUtil.name, new TokenUtil(config.twitchAppClientId));
+    }
+
+    public async initManageableClasses() {
+        for (const manageableClass of this.manageableClasses.values()) {
+            await manageableClass.init();
+        }
+    }
+
+    public getInstance(name: string): ManageableClass | undefined {
+        return this.manageableClasses.get(name);
+    }
+
+}
