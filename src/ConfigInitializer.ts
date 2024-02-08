0dcd49407c1baa21a44d1fd6dea15371cbe7ef8a
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-08 13:21:55 +0100

    Add a ConfigInitilizer class

---
+++ src/ConfigInitializer.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface Config {
+    readonly twitchAppClientId: string;
+    readonly twitchAppClientSecret: string;
+    readonly httpServerPort: string;
+}
+
+export default class ConfigInitializer {
+    private config: Config;
+
+    constructor() {
+        this.config = {
+            twitchAppClientId: process.env.TWITCH_CLIENT_ID || "",
+            twitchAppClientSecret: process.env.TWITCH_CLIENT_SECRET || "",
+            httpServerPort: process.env.HTTP_SERVER_PORT || "",
+        }
+    }
+
+    getConfig(): Config {
+        return this.config;
+    }
+
+    checkConfigurationVariables(): never | void {
+        for (const key in this.config) {
+           const value = this.config[key as keyof Config];
+           if(!value) throw Error(`The ${key as keyof Config} enviroment variable is not set :C`);
+        }
+    }
+}
