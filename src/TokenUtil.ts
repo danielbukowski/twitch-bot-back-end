e713f60afd8c27ccba3ac9e50b82a5095da11cea
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-09 20:03:17 +0100

    Refactor the TokenUtil class

--- src/TokenUtil.ts
+++ src/TokenUtil.ts
@@ -1,40 +1,45 @@
-import { HelixUser } from "@twurple/api";
 import { AccessToken } from "@twurple/auth";
 import axios from "axios";
-import { readFileSync, writeFileSync } from "fs";
-import { join } from "path";
+import ManageableClass from "./ManageableClass";
 
-export default class TokenUtil {
-  private readonly PATH_TO_TOKEN = "../authToken.json";
+interface TwitchUser {
+  id: string;
+  login: string;
+  display_name: string;
+  type: string;
+  broadcaster_type: string;
+  description: string;
+  profile_image_url: string;
+  offline_image_url: string;
+  view_count: number;
+  email: string;
+  created_at: string;
+}
 
+export default class TokenUtil implements ManageableClass{
+  private clientId: string;
 
   public constructor(clientId: string) {
     this.clientId = clientId;
   }
-
-  public readAccessTokenFromFile(): AccessToken {
-    return JSON.parse(
-      readFileSync(join(__dirname, this.PATH_TO_TOKEN), "utf-8")
-    ) as AccessToken;
+  
+  async init(): Promise<void> {
+    console.log("Initializing the TokenUtil...");
+    console.log("Initialized the TokenUtil!");
   }
 
-  public writeAccessTokenToFile(accessToken: AccessToken) {
-    writeFileSync(
-      join(__dirname, this.PATH_TO_TOKEN),
-      JSON.stringify(accessToken)
-    );
-  }
-  public async getUsernameByAccessToken(accessToken: AccessToken): Promise<string> {
-    const userInfo = await axios.get<{ data: HelixUser[] }>(
+  public async getUsernameByAccessToken(accessToken: AccessToken): Promise<string | undefined> {
+    const userInfo = await axios.get<{ data: TwitchUser[] }>(
       "https://api.twitch.tv/helix/users",
       {
         headers: {
           "Client-Id": this.clientId,
-          Authorization: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${accessToken.accessToken}`,
           "Content-Type": "application/json",
         },
       }
-    );
-    return userInfo.data.data[0].displayName.toLowerCase();
+    ).catch(() => { return undefined });
+
+    return userInfo?.data.data[0].display_name.toLowerCase();
   }
 }
