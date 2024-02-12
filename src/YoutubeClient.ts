40a8b38f5b439e3ac42fced683ca2b159f571325
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-12 22:20:59 +0100

    Add a method to get a video from YouTube by a name in the YoutubeClient class

--- src/YoutubeClient.ts
+++ src/YoutubeClient.ts
@@ -12,4 +12,25 @@ export default class YoutubeClient implements ManageableClass{
     console.log("Initialized the YoutubeClient...");
   }
 
+  async getVideoIdByName(name: string): Promise<string> {
+    return await axios
+      .get<{
+        items: [
+          {
+            id: {
+              videoId: string;
+            };
+          }
+        ];
+      }>("https://youtube.googleapis.com/youtube/v3/search", {
+        params: {
+          maxResults: 1,
+          part: "id",
+          fields: "items/id/videoId",
+          q: name,
+          key: this.youtubeApiKey,
+        },
+      })
+      .then((response) => response.data.items[0].id.videoId);
+  }
 }
