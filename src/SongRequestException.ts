47c35f085c27336db2b5b33c35148788d74acfbe
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-16 18:52:56 +0100

    Remove a wrong named class

--- src/SongRequestException.ts
+++
@@ -1,7 +0,0 @@
-export default class SongRequestException extends Error {
-
-    public constructor(message: string) {
-        super(message);
-        this.name = "SongRequestException";
-    }
-}
