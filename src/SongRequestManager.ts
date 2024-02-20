7a84f814941c489e751790b3ac0d881e51a2ef66
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-20 22:21:54 +0100

    Add a method to add a song to the queue

--- src/SongRequestManager.ts
+++ src/SongRequestManager.ts
@@ -15,4 +15,16 @@ export default class SongRequestManager implements ManageableClass {
         console.log("Initializing the SongRequestClient...");
         console.log("Initialized the SongRequestClient!");
     }
+
+    public addSongToQueue(song: Song) {
+        const length: number = this.songQueue.unshift(song);
+        const duration: number = [...this.songQueue]
+          .map((s) => s.durationInSeconds)
+          .reduce((acc: number, currDuration: number) => acc + currDuration, 0);
+
+        return {
+            length,
+            duration
+        }
+    }
 }
