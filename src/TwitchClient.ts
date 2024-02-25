b71b3c462aad8caa4e9fc136a9bb2bcb4221f5b8
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-25 18:16:27 +0100

    Add a TwitchClient class

---
+++ src/TwitchClient.ts
@@ -0,0 +1,22 @@
+import { ApiClient } from "@twurple/api";
+import ManageableClass from "./ManageableClass";
+import { RefreshingAuthProvider } from "@twurple/auth";
+
+export default class TwitchClient implements ManageableClass  {
+    private apiClient!: ApiClient;
+
+    constructor(private authProvider: RefreshingAuthProvider) {
+        this.apiClient = new ApiClient({
+            authProvider: this.authProvider
+        });
+    }
+
+    async init(): Promise<void> {
+        console.log("Initializing the TwitchClient...");
+        console.log("Initialized the TwitchClient...");
+    }
+
+    public getApiClient(): ApiClient {
+        return this.apiClient;
+    }
+}
