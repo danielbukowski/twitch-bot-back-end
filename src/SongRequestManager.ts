02bb8ac4155b5d90da0dcfa9b0512a277d63bf75
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-25 18:36:21 +0100

    Change a method to push to the queue

--- src/SongRequestManager.ts
+++ src/SongRequestManager.ts
@@ -17,7 +17,7 @@ export default class SongRequestManager implements ManageableClass {
     }
 
     public addSongToQueue(song: Song) {
-        const length: number = this.songQueue.unshift(song);
+        const length: number = this.songQueue.push(song);
         const duration: number = [...this.songQueue]
           .map((s) => s.durationInSeconds)
           .reduce((acc: number, currDuration: number) => acc + currDuration, 0);
