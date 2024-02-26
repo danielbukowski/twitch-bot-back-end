dd3522fe92edf600c4101af65ffec22dfe825d24
danielbukowski <daniel.bukowski01@gmail.com>
2024-02-26 21:15:43 +0100

    Delete the TwitchUser interface

--- src/TokenUtil.ts
+++ src/TokenUtil.ts
@@ -2,19 +2,6 @@ import { AccessToken } from "@twurple/auth";
 import axios from "axios";
 import ManageableClass from "./ManageableClass";
 
-interface TwitchUser {
-  id: string;
-  login: string;
-  display_name: string;
-  type: string;
-  broadcaster_type: string;
-  description: string;
-  profile_image_url: string;
-  offline_image_url: string;
-  view_count: number;
-  email: string;
-  created_at: string;
-}
 
 export default class TokenUtil implements ManageableClass {
 
