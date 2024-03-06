c2827f2fd7105b5822b8c6cb9676179ed6735f20
danielbukowski <daniel.bukowski01@gmail.com>
2024-03-06 01:01:57 +0100

    Refactor the class and add 2 methods to handle different subscription events

--- src/TwitchEventListener.ts
+++ src/TwitchEventListener.ts
@@ -1,8 +1,6 @@
 import { EventSubWsListener } from "@twurple/eventsub-ws";
 import ManageableClass from "./ManageableClass";
 import SocketClient from "./SocketClient";
-import { ApiClient } from "@twurple/api";
-import { RefreshingAuthProvider } from "@twurple/auth";
 import TwitchClient from "./TwitchClient";
 
 export default class TwitchEventListener implements ManageableClass {
@@ -13,8 +11,30 @@ export default class TwitchEventListener implements ManageableClass {
     private readonly twitchClient: TwitchClient
   ) { }
 
-    async init(): Promise<void> {
-        console.log("Initializing the TwitchEventListener...");
-        console.log("Initialized the TwitchEventListener...");
-    }
+  async init(): Promise<void> {
+    console.log("Initializing the TwitchEventListener...");
+
+    this.eventSub = new EventSubWsListener({
+      apiClient: this.twitchClient.getApiClient(),
+    });
+
+    const channelId: string = await this.twitchClient.getIdFromAccessTokenForIntent("events");
+
+    this.eventSub.onChannelSubscription(channelId, (e) => {
+      const userName = e.userDisplayName;
+
+      this.socketClient.handleFirstSubscription(userName);
+    });
+
+    this.eventSub.onChannelSubscriptionMessage(channelId, (e) => {
+      const userName = e.userDisplayName;
+      const subStreakInMonths = e.streakMonths ?? 0;
+
+      this.socketClient.handleResubscription(userName, subStreakInMonths)
+    });
+
+    this.eventSub.start();
+
+    console.log("Initialized the TwitchEventListener!");
+  }
 }
