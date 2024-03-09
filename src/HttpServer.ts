d566e35bee8c22c268eda35ae8af07550b360660
danielbukowski <daniel.bukowski01@gmail.com>
2024-03-09 23:38:00 +0100

    Add CORS to the HttpServer

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import ManageableClass from "./ManageableClass";
 import { createServer, Server } from "node:http";
+import cors from "cors";
 
 export default class HttpServer implements ManageableClass {
   private app = express();
@@ -16,6 +17,15 @@ export default class HttpServer implements ManageableClass {
 
   async init(): Promise<void> {
     console.log("Initializing the HttpServer...");
+
+    this.app.use(
+      cors({
+        //origin to dev Vue.js
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+      }),
+    );
 
     this.httpServer.listen(this.port, () => {
       console.log(`Server is running at http://localhost:${this.port}`);
