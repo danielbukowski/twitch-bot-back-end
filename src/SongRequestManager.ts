6c587c1e4a963ffd9ed9bf992d8c50e81bacf384
danielbukowski <daniel.bukowski01@gmail.com>
2024-03-17 23:58:11 +0100

    Remove duration and length return types from the method

--- src/SongRequestManager.ts
+++ src/SongRequestManager.ts
@@ -21,16 +21,8 @@ export default class SongRequestManager implements ManageableClass {
     console.log("Initialized the SongRequestClient!");
   }
 
-  public addSongToQueue(song: Song) {
-    const length: number = this.songQueue.push(song);
-    const duration: number = [...this.songQueue]
-      .map((s) => s.durationInSeconds)
-      .reduce((acc: number, currDuration: number) => acc + currDuration, 0);
-
-    return {
-      length,
-      duration,
-    };
+  public addSongToQueue(song: Song): void {
+    this.songQueue.push(song);
   }
 
   public removeSongFromQueue(): Song | undefined {
