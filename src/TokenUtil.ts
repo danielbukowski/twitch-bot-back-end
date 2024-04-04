5591f77b084ed227a8580aac928c721c50e33452
danielbukowski <daniel.bukowski01@gmail.com>
2024-04-04 22:32:02 +0200

    Add a method to encrypt tokens

--- src/TokenUtil.ts
+++ src/TokenUtil.ts
@@ -1,11 +1,19 @@
 import { AccessToken } from "@twurple/auth";
 import ManageableClass from "./ManageableClass";
 import { readFile, readdir } from "fs/promises";
+import { createCipheriv, pbkdf2Sync, randomBytes, CipherGCM, CipherGCMTypes } from "crypto";
 
 export type TokenIntent = "events" | "chat";
 
 export default class TokenUtil implements ManageableClass {
   private readonly regexToGetIdFromToken: RegExp = /[-\\.]/;
+  private readonly ALGORITHM_NAME: CipherGCMTypes = "aes-128-gcm";
+  private readonly ALGORITHM_NONCE_SIZE: number = 12;
+  private readonly ALGORITHM_TAG_SIZE: number = 16;
+  private readonly ALGORITHM_KEY_SIZE: number = 16;
+  private readonly PBKDF2_NAME: string = "sha256";
+  private readonly PBKDF2_SALT_SIZE: number = 18;
+  private readonly PBKDF2_ITERATIONS: number = 33217;
 
   public constructor(private readonly clientId: string) {}
 
@@ -39,5 +47,19 @@ export default class TokenUtil implements ManageableClass {
         { encoding: "utf-8" },
       ),
     ) as AccessToken;
+  }
+
+  private encryptPlainToken(plainToken: string): String {
+    const salt = randomBytes(this.PBKDF2_SALT_SIZE);
+    const key: Buffer = pbkdf2Sync(Buffer.from(this.encryptionKey, "utf8"), salt, this.PBKDF2_ITERATIONS, this.ALGORITHM_KEY_SIZE, this.PBKDF2_NAME);
+    const iv: Buffer = randomBytes(this.ALGORITHM_NONCE_SIZE);
+
+    const cipher: CipherGCM = createCipheriv(this.ALGORITHM_NAME, key, iv);
+
+    const ciphertext = Buffer.concat([ cipher.update(Buffer.from(plainToken, "utf-8")), cipher.final() ]);
+
+    const saltNonceCipertextAndTag = Buffer.concat([ salt, iv, ciphertext, cipher.getAuthTag()]);
+
+    return saltNonceCipertextAndTag.toString("base64");
   }
 }
