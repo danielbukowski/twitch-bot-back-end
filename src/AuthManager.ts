c8063795e33eea0f23b1acec993531419f4e1b13
danielbukowski <daniel.bukowski01@gmail.com>
2024-04-10 15:58:34 +0200

    Refactor the AuthManager class

--- src/AuthManager.ts
+++ src/AuthManager.ts
@@ -1,5 +1,4 @@
 import { AccessToken, RefreshingAuthProvider } from "@twurple/auth";
-import { writeFile } from "fs/promises";
 import { Initializable } from "./ObjectManager";
 import TokenUtil, { TokenIntent } from "./TokenUtil";
 
@@ -24,34 +23,20 @@ export default class AuthManager implements Initializable {
   public async init(): Promise<void> {
     console.log("Initializing the AuthManager...");
 
-    await this.initTokenForIntent("chat");
-    await this.initTokenForIntent("events");
-
-    const idOfAccessTokenForChat: string =
-      await this.tokenUtil.findIdOfAccessTokenInDirectory("chat");
-
     this.authProvider.onRefresh(
-      async (userId: string, newTokenData: AccessToken) =>
-        await writeFile(
-          `./access-tokens/${idOfAccessTokenForChat === userId ? "chat" : "events"}accessToken-${userId}.json`,
-          JSON.stringify(newTokenData, null, 4),
-          { encoding: "utf-8" },
-        ),
-    );
+      async (userId: string, newToken: AccessToken) => {
+        const scopesOfNewToken = newToken.scope;
 
+        const tokenIntent: TokenIntent | undefined = this.tokenUtil.checkUseOfScope(scopesOfNewToken);
+
+        if(!tokenIntent) {
+          throw new Error("An error occured when refreshing an access token");
+        }
+
+        await this.tokenUtil.writeAccessTokenToDirectory(newToken, tokenIntent);
+       }
+     );
+  
     console.log("Initialized the AuthManager!");
-  }
-
-  private async initTokenForIntent(tokenIntent: TokenIntent): Promise<void> {
-    const accessTokenIdForIntent: string =
-      await this.tokenUtil.findIdOfAccessTokenInDirectory(tokenIntent);
-    const accessTokenForIntent: AccessToken =
-      await this.tokenUtil.readAccessTokenFromDirectory(
-        accessTokenIdForIntent,
-        tokenIntent,
-      );
-    await this.authProvider.addUserForToken(accessTokenForIntent, [
-      tokenIntent,
-    ]);
   }
 }
