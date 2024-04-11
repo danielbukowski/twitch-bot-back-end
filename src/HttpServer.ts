d5dee40e674c93d7eb2278e41c4f05cc6433f9f9
danielbukowski <daniel.bukowski01@gmail.com>
2024-04-11 23:10:22 +0200

    Add an oauth2 callback endpoint

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Initializable } from "./ObjectManager";
 import { createServer, Server } from "node:http";
 import cors from "cors";
+import TokenUtil, { TokenIntent } from "./TokenUtil";
+import { AccessToken } from "@twurple/auth";
 import TwitchClient from "./TwitchClient";
 
 export default class HttpServer implements Initializable {
@@ -31,6 +33,38 @@ export default class HttpServer implements Initializable {
         credentials: true,
       }),
     );
+
+    this.app.get("/api/v1/oauth2/callback", async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const {code} = req.query;
+
+        if(!code || typeof code !== "string") {
+          res.status(400).json({
+            status: 400,
+            message: "The code in query parameters must not be empty"
+          })
+          return;
+        }
+      
+        const accessToken: AccessToken = await this.twitchClient.exchangeCodeToAccessToken(code);
+        const tokenIntent: TokenIntent | undefined = this.tokenUtil.checkUseOfScopes(accessToken.scope);
+
+        if(!tokenIntent) {
+          res.status(400).json({
+            status: 400,
+            message: "The provided scopes do not match to any use"
+          })
+          return;
+        }
+      
+        this.tokenUtil.writeAccessTokenToDirectory(accessToken, tokenIntent);
+
+        res.sendStatus(200);
+      } catch (e: unknown) {
+        next(e);
+    }
+  });
+  
 
     this.httpServer.listen(this.port, () => {
       console.log(`Server is running at http://localhost:${this.port}`);
