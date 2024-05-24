b4593007bcaa08fc607bc9d3a43d14e19776447a
danielbukowski <daniel.bukowski01@gmail.com>
2024-05-24 22:20:05 +0200

    Add a TextToSpeech class

---
+++ src/TextToSpeech.ts
@@ -0,0 +1,45 @@
+import type { Namespace, Server as SocketIO } from "socket.io";
+import type { Initializable } from "./ObjectManager";
+import {
+	HasRole,
+	type BasicCommand,
+	type CommandContainer,
+} from "./TwitchChat";
+import type { ChatClient, ChatUser } from "@twurple/chat";
+import Logger from "./Logger";
+
+export default class TextToSpeech implements Initializable, CommandContainer {
+	constructor(private readonly socketIO: SocketIO) {}
+
+	public getCommands(): Map<string, BasicCommand> {
+		const commands = new Map<string, BasicCommand>();
+
+		commands.set("tts", this.playTextToSpeech.bind(this));
+
+		return commands;
+	}
+
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	private getNamespace(): Namespace<any> {
+		return this.socketIO.of("/text-to-speech");
+	}
+
+	public async init(): Promise<void> {
+		Logger.info(`Initializing the ${this.constructor.name}...`);
+		Logger.info(`Initialized the ${this.constructor.name}!`);
+	}
+
+	@HasRole(["Broadcaster", "Mod"])
+	private async playTextToSpeech(
+		chatClient: ChatClient,
+		channelName: string,
+		commandParameters: string[],
+		userInfo: ChatUser,
+	): Promise<void> {
+		const text = commandParameters.join(" ");
+
+		this.getNamespace().emit("text-to-speech-message", {
+			text,
+		});
+	}
+}
