df2dc7d43d9cc2379f72477166c2580427515cdf
danielbukowski <daniel.bukowski01@gmail.com>
2024-06-19 23:18:20 +0200

    Wrap YouTubeClient class's methods with logs and rename some variables

--- src/YouTubeClient.ts
+++ src/YouTubeClient.ts
@@ -24,7 +24,14 @@ export default class YouTubeClient implements Initializable {
 	}
 
 	public async getSongIdByName(name: string): Promise<string | undefined> {
+		Logger.trace("Entering getSongIdByName() method");
+		Logger.debug("GetSongIdByName method's parameters", {
+			parameters: {
+				name,
+			},
+		});
 		try {
+			Logger.trace("Making a GET API call");
 			const response = await axios.get<{
 				items: [
 					{
@@ -42,21 +49,43 @@ export default class YouTubeClient implements Initializable {
 					key: this.youtubeApiKey,
 				},
 			});
+
+			Logger.debug("API call response", {
+				status: response.statusText,
+				data: response.data,
+			});
+
 			return response.data.items[0].id.videoId;
-		} catch (error) {
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				Logger.debug("API call threw error", {
+					errorName: error.name,
+					errorMessage: error.message,
+					stack: error.stack,
+				});
+			}
 			return undefined;
+		} finally {
+			Logger.trace("Exiting getSongIdByName() method");
 		}
 	}
 
 	public async getSongDetailsById(
-		videoId: string,
+		songId: string,
 	): Promise<SongDetails | undefined> {
+		Logger.trace("Entering getSongDetailsById() method");
+		Logger.debug("GetSongDetailsById method's parameters", {
+			parameters: {
+				songId,
+			},
+		});
 		try {
+			Logger.trace("Making a GET API call");
 			const response = await axios.get<{ items: SongDetails[] }>(
 				"https://www.googleapis.com/youtube/v3/videos",
 				{
 					params: {
-						id: videoId,
+						id: songId,
 						key: this.youtubeApiKey,
 						fields:
 							"items(snippet/title,contentDetails/duration,statistics/viewCount)",
@@ -65,20 +94,42 @@ export default class YouTubeClient implements Initializable {
 				},
 			);
 
+			Logger.debug("API call response", {
+				status: response.statusText,
+				data: response.data,
+			});
+
 			//YouTube Data API likes to return an empty list with 200 status code
 			if (!response.data.items.length)
-				throw new Error("YouTube Data API has returned an empty item list");
+				throw new Error("YouTube Data API returned an empty item list");
 
 			return response.data.items[0];
 		} catch (error: unknown) {
+			if (error instanceof Error) {
+				Logger.debug("API call threw error", {
+					errorName: error.name,
+					errorMessage: error.message,
+					stack: error.stack,
+				});
+			}
 			return undefined;
+		} finally {
+			Logger.trace("Exiting getSongDetailsById() method");
 		}
 	}
 
 	public async downloadYouTubeAudio(
 		audioId: string,
 	): Promise<string | undefined> {
-		const { promise, resolve, reject } = Promise.withResolvers<string | undefined>();
+		Logger.trace("Entering downloadYouTubeAudio() method");
+		Logger.debug("DownloadYouTubeAudio method's parameters", {
+			parameters: {
+				audioId,
+			},
+		});
+		const { promise, resolve, reject } = Promise.withResolvers<
+			string | undefined
+		>();
 		const buffers: Buffer[] = [];
 
 		ytdl(`https://www.youtube.com/watch?v=${audioId}`, {
@@ -90,12 +141,27 @@ export default class YouTubeClient implements Initializable {
 			})
 			.on("end", () => {
 				const data = Buffer.concat(buffers);
+
+				Logger.debug("Ended fetching audio chunks", {
+					audioId,
+					fetchedChunks: buffers.length,
+				});
+
 				resolve(`data:audio/mp3;base64,${data.toString("base64")}`);
 			})
-			.on("error", () => {
+			.on("error", (err) => {
+				Logger.error("Failed to fetch audio from YouTube", {
+					audioId,
+					fetchedChunks: buffers.length,
+					errorMessage: err.message,
+					errorName: err.name,
+					stack: err.stack,
+				});
+
 				reject(undefined);
 			});
 
+		Logger.trace("Exiting downloadYouTubeAudio() method");
 		return promise;
 	}
 }
