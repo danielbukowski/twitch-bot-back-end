678de486f53ed8db3be6b62c69410f86942d9b7c
danielbukowski <daniel.bukowski01@gmail.com>
2024-07-04 23:37:30 +0200

    Replace parsing the environment variables using zod

--- src/ConfigInitializer.ts
+++ src/ConfigInitializer.ts
@@ -1,14 +1,15 @@
 import * as Logger from "./Logger";
+import * as z from "zod";
 
-interface EnvironmentVariables {
-	TWITCH_APP_CLIENT_ID: string;
-	TWITCH_APP_CLIENT_SECRET: string;
-	HTTP_SERVER_PORT: string;
-	YOUTUBE_API_KEY: string;
-	ENCRYPTION_PASSPHRASE: string;
-	OAUTH2_REDIRECT_URI: string;
-	FRONTEND_ORIGIN: string;
-}
+const ENVIRONMENT_VARIABLES_SCHEMA = z.object({
+	TWITCH_APP_CLIENT_ID: z.string(),
+	TWITCH_APP_CLIENT_SECRET: z.string(),
+	HTTP_SERVER_PORT: z.string(),
+	YOUTUBE_API_KEY: z.string(),
+	ENCRYPTION_PASSPHRASE: z.string(),
+	OAUTH2_REDIRECT_URI: z.string(),
+	FRONTEND_ORIGIN: z.string(),
+});
 
 export type UserType =
 	| "Mod"
@@ -19,37 +20,27 @@ export type UserType =
 	| "Founder";
 
 export default class ConfigInitializer {
-	private environmentVariables: Readonly<EnvironmentVariables>;
+	private environmentVariables!: z.infer<typeof ENVIRONMENT_VARIABLES_SCHEMA>;
 
-	constructor() {
-		this.environmentVariables = {
-			TWITCH_APP_CLIENT_ID: process.env.TWITCH_APP_CLIENT_ID || "",
-			TWITCH_APP_CLIENT_SECRET: process.env.TWITCH_APP_CLIENT_SECRET || "",
-			HTTP_SERVER_PORT: process.env.HTTP_SERVER_PORT || "",
-			YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || "",
-			ENCRYPTION_PASSPHRASE: process.env.ENCRYPTION_PASSPHRASE || "",
-			OAUTH2_REDIRECT_URI: process.env.OAUTH2_REDIRECT_URI || "",
-			FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN || "",
-		};
-	}
-
-	public getEnvironmentVariables(): EnvironmentVariables {
+	public getEnvironmentVariables(): z.infer<
+		typeof ENVIRONMENT_VARIABLES_SCHEMA
+	> {
 		return this.environmentVariables;
 	}
 
 	public checkEnvironmentVariables(): void {
 		Logger.info("Checking the environment variables...");
 
-		for (const key in this.environmentVariables) {
-			const value =
-				this.environmentVariables[key as keyof EnvironmentVariables];
-			if (!value)
-				throw new Error(
-					`The ${
-						key as keyof EnvironmentVariables
-					} environment variable is not set :C`,
-				);
+		const parsedEnvironmentVariables = ENVIRONMENT_VARIABLES_SCHEMA.safeParse(
+			process.env,
+		);
+
+		if (parsedEnvironmentVariables.error) {
+			Logger.error(parsedEnvironmentVariables.error);
+			throw new Error("Failed to parse the environment variables");
 		}
+
+		this.environmentVariables = parsedEnvironmentVariables.data;
 
 		Logger.info("The environment variables are checked!");
 	}
